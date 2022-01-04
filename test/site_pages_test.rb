55be7f7ff9a6f5cf0ab3ed46fd2bf86986dd5bc0
dawidl022 <domdaw7@gmail.com>
2022-01-04 05:36:18 +0000

    Add page navigation

--- test/site_pages_test.rb
+++ test/site_pages_test.rb
@@ -3,6 +3,10 @@ require_relative 'test_helper'
 class PalindromeAppTest < Minitest::Test
   include Rack::Test::Methods
 
+  def setup
+    @base_title = "Learn Enough Ruby Sample App |"
+  end
+
   def app
     Sinatra::Application
   end
@@ -10,15 +14,19 @@ class PalindromeAppTest < Minitest::Test
   def test_index
     get '/'
     test_page_exists
+    assert_equal "#{@base_title} Home", title_content
+    test_nav
   end
 
   def test_about
     get '/about'
     test_page_exists
+    assert_equal "#{@base_title} About", title_content
   end
 
   def test_palindrome
     get '/palindrome'
     test_page_exists
+    assert_equal "#{@base_title} Palindrome Detector", title_content
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -16,3 +16,11 @@ def test_page_exists
   assert last_response.ok?
   assert doc(last_response).at_css('h1')
 end
+
+def title_content
+  doc(last_response).at_css('title').content
+end
+
+def test_nav
+  assert doc(last_response).at_css('nav')
+end
